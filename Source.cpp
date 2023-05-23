892a5e93bf0214f034c6759532dc92c4e818f1cb
Marjazh-hub <126614526+Marjazh-hub@users.noreply.github.com>
2023-05-24 00:35:40 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -2,7 +2,7 @@
 #include<ctime>
 #include <cstdlib>
 
-int main() { // функци€ main должна иметь возвращаемый тип int
+int main() {
     setlocale(LC_ALL, "ru");
     const int ROWS = 3;
     const int COLS = 3;
@@ -12,10 +12,10 @@ int main() { // функци€ main должна иметь возвращаемый тип int
     srand(time(NULL));
     for (int i = 0; i < ROWS; i++) {
         for (int j = 0; j < COLS; j++) {
-            arr[i][j] = rand() % 9; // записываем сгенерированное случайное число в €чейку массива
+            arr[i][j] = rand() % 9;
         }
     }
-    std::cout << arr[ROWS - 1][COLS - 1] << std::endl; // выводим последний элемент массива
+    std::cout << arr[ROWS - 1][COLS - 1] << std::endl;
 
-    return 0; // возвращаем 0, чтобы показать корректное завершение программы
+    return 0;
 }
